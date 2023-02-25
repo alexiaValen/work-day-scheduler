754e06eb018ddbcbec08161a44ba2084766d6d5b
Alexia Valenzuela <alexiavalenzuela@Alexias-MacBook-Air.local>
2023-02-25 11:49:14 -0700

    past, present, future assigned to divs with dayjs code

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,31 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function() {
+    console.log("finished.");
+});
+
+
+let currentHour = dayjs().hour();
+//Past, present, and future functionality -
+    //Compare timeblock time with current time to determine whether
+    //activity is in the past, present, or future 
+    $(".time-div").each(function() {
+        var timeDiv = $(this).attr("id").split("-")[1];
+
+        if (currentHour == timeDiv) {
+            $(this).addClass("present");
+            $(this).children(".description").addClass("present");
+
+        } else if (currentHour < timeDiv) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+
+        } else if (currentHour > timeDiv) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
 
 
 $(function () {
